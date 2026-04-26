8c0bbaed91ddc8f1a525d28f53ece09795242717
dmswjdals112-commit <dmswjdals112@gmail.com>
2026-04-26 17:37:18 +0900

    프로그래머스 신규 아이디 추천 - 해결 코드 커밋

---
+++ 신규 아이디 추천/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+
+        if (answer.equals("")) {
+            answer = "a";
+        }
+
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$", "");
+        }
+
+        while (answer.length() <= 2) {
+            answer += answer.charAt(answer.length() - 1);
+        }
+
+        return answer;
+    }
+}
